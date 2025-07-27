5ff251ed4a42d1600c7fc7a5374cf40c2aa06c10
jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>
2025-07-27 07:34:00 +0200

    feat: add Update method stub to MemorySystem to prepare for system updates
    Signed-off-by: jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>

--- JLifeEngine/Core/Life/Systems/MemorySystem.cs
+++ JLifeEngine/Core/Life/Systems/MemorySystem.cs
@@ -4,4 +4,8 @@ public class MemorySystem : ILifeSystem
 {
     // Store and retrieve being memories
     // Influence decisions based on past experiences
+    public void Update(TimeSpan deltaTime, World world)
+    {
+        throw new NotImplementedException();
+    }
 }
