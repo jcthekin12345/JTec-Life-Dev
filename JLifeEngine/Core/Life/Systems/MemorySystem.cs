b78caf77b4242d0be714adafcf1ce4502a27a228
jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>
2025-07-27 07:26:16 +0200

    feat: add MemorySystem implementing ILifeSystem
    Signed-off-by: jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>

---
+++ JLifeEngine/Core/Life/Systems/MemorySystem.cs
@@ -0,0 +1,7 @@
+namespace JLifeEngine.Core.Life.Systems;
+
+public class MemorySystem : ILifeSystem
+{
+    // Store and retrieve being memories
+    // Influence decisions based on past experiences
+}
