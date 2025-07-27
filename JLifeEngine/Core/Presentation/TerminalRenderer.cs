95123330798bcf84e6828e9a30c156111fa30721
jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>
2025-07-27 07:27:18 +0200

    feat: add RenderWorld method and RenderBeingStatus helper in TerminalRenderer class
    Signed-off-by: jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>

--- JLifeEngine/Core/Presentation/TerminalRenderer.cs
+++ JLifeEngine/Core/Presentation/TerminalRenderer.cs
@@ -2,5 +2,26 @@ namespace JLifeEngine.Core.Presentation;
 
 public class TerminalRenderer
 {
-    
+    public void RenderWorld(World world, ViewMode mode)
+    {
+        switch (mode)
+        {
+            case ViewMode.Overview:
+                RenderOverview(world);
+                break;
+            case ViewMode.FocusedBeing:
+                RenderBeingFocus(world.SelectedBeing);
+                break;
+            case ViewMode.Location:
+                RenderLocation(world.CurrentLocation);
+                break;
+        }
+    }
+
+    private void RenderBeingStatus(Being being)
+    {
+        Console.ForegroundColor = GetBeingMoodColor(being);
+        Console.WriteLine($"{being.Name}: {being.CurrentAction?.Description ?? "Idle"}");
+        // Show needs, relationships, current thoughts
+    }
 }
