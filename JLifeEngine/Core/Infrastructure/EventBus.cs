a1bcfdba9a992daa5664816c9f510fadca238d47
jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>
2025-07-27 07:30:04 +0200

    feat: add LifeEventOccurred event and PublishEvent method to EventBus in main
    Signed-off-by: jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>

--- JLifeEngine/Core/Infrastructure/EventBus.cs
+++ JLifeEngine/Core/Infrastructure/EventBus.cs
@@ -2,5 +2,11 @@ namespace JLifeEngine.Core.Infrastructure;
 
 public class EventBus
 {
+    public event Action<LifeEvent> LifeEventOccurred;
     
+    public void PublishEvent(LifeEvent evt)
+    {
+        LifeEventOccurred?.Invoke(evt);
+        // Log to history, trigger sound effects, update UI
+    }
 }
