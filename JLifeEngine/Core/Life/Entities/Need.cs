375eff25ba7a92ffbdb76f130287928da0349c17
jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>
2025-07-27 07:23:47 +0200

    feat: make Need an abstract class and add derived need types

--- JLifeEngine/Core/Life/Entities/Need.cs
+++ JLifeEngine/Core/Life/Entities/Need.cs
@@ -1,6 +1,12 @@
 namespace JLifeEngine.Core.Life.Entities;
 
-public class Need
+public abstract class Need
 {
-    
+    public float Intensity { get; set; } // 0.0 to 1.0
+    public DateTime LastSatisfied { get; set; }
+    public abstract bool ShouldActOn();
 }
+
+public class HungerNeed : Need { }
+public class SocialNeed : Need { }
+public class RestNeed : Need { }
