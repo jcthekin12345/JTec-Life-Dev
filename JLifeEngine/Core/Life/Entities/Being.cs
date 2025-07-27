feb9590a51e70e048bb38d93b259f60ee796bbf2
jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>
2025-07-27 07:23:28 +0200

    feat: extend Being class with additional properties

--- JLifeEngine/Core/Life/Entities/Being.cs
+++ JLifeEngine/Core/Life/Entities/Being.cs
@@ -2,5 +2,12 @@ namespace JLifeEngine.Core.Life.Entities;
 
 public class Being
 {
-    
+    public BeingId Id { get; set; }
+    public List<Need> Needs { get; set; }
+    public List<Trait> Traits { get; set; }
+    public List<Relationship> Relationships { get; set; }
+    public List<Memory> Memories { get; set; }
+    public Action? CurrentAction { get; set; }
+    public Location Location { get; set; }
+    public DateTime LastUpdate { get; set; }
 }
