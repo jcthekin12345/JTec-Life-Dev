089f7a454df9cbc9f17e63fd63e08e17d09f1657
jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>
2025-07-27 07:28:25 +0200

    feat: add SaveWorldAsync and LoadWorldAsync methods to WorldSaveManager in main
    Signed-off-by: jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>

--- JLifeEngine/Core/Persistence/WorldSaveManager.cs
+++ JLifeEngine/Core/Persistence/WorldSaveManager.cs
@@ -2,5 +2,24 @@ namespace JLifeEngine.Core.Persistence;
 
 public class WorldSaveManager
 {
-    
+    public async Task SaveWorldAsync(World world, string filePath)
+    {
+        var saveData = new WorldSaveData
+        {
+            Beings = world.Beings.ToList(),
+            Locations = world.Locations.ToList(),
+            GlobalEvents = world.EventHistory.ToList(),
+            Timestamp = DateTime.UtcNow
+        };
+
+        var json = JsonSerializer.Serialize(saveData, _serializerOptions);
+        await File.WriteAllTextAsync(filePath, json);
+    }
+
+    public async Task<World> LoadWorldAsync(string filePath)
+    {
+        var json = await File.ReadAllTextAsync(filePath);
+        var saveData = JsonSerializer.Deserialize<WorldSaveData>(json, _serializerOptions);
+        return RestoreWorld(saveData);
+    }
 }
