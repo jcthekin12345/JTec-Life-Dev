6ce1e63a9128707112e56a28d31dc8eea0c3c261
jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>
2025-07-27 07:26:26 +0200

    feat: implement ILifeSystem in NeedSystem and add Update method
    Signed-off-by: jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>

--- JLifeEngine/Core/Life/Systems/NeedSystem.cs
+++ JLifeEngine/Core/Life/Systems/NeedSystem.cs
@@ -1,6 +1,10 @@
 namespace JLifeEngine.Core.Life.Systems;
 
-public class NeedSystem
+public class NeedSystem : ILifeSystem
 {
-    
+    public void Update(TimeSpan deltaTime, World world)
+    {
+        // Update all being needs over time
+        // Trigger actions when needs become urgent
+    }
 }
